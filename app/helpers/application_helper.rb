761386b4d441a818d7d360780a05a8ecad8f82f4
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-03 22:47:45 +0300

    Finished Chapter 5.5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
 
+# Copied here again in Section 5.4.2 
+def full_title(page_title)
+  base_title = 'Ruby on Rails Training'
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
 
 end
