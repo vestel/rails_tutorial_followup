4797a9321a41e2f208ddb76c18c55f417223a37f
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-03 23:00:26 +0300

    Starting Chapter 6

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,2 @@
+include ApplicationHelper
 
-# Moved here in Section 5.3.4 
-def full_title(page_title)
-  base_title = 'Ruby on Rails Training'
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
