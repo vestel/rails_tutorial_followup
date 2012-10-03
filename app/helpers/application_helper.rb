5dbefbeb75a88719db949cf7d7c5d1f46766cd1f
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-03 16:20:54 +0300

    Finished Chapter 5.3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
 
-  # Return full title for each page
-  def full_title(page_title)
-    base_title = 'Ruby on Rails Training'
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,56 +1,50 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
     it "should have the content 'TrainingApp'" do
-      visit root_path
       page.should have_content('Training App')
     end
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_content('Ruby on Rails Training')
-    end
+    it { should have_selector('h1', text: 'Training App') }
 
-    it "should not have the custom title" do
-      visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should_not have_selector('title', text: full_title('| Home')) }
   end
 
   describe "Help page" do
-    it "should have the header 'Help Page'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help Page')
-      page.should have_selector('title', :text => 'Ruby on Rails Training | Help Page')
-    end
+    before { visit help_path }
+    it {
+      should have_selector('h1', text: 'Help Page')
+      should have_selector('title', text: full_title('Help Page'))
+    }
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the content 'About us'" do
-      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the header 'About Us'" do
-      visit about_path 
       page.should have_selector('h1', text: 'About Us')
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the title 'Contact page'" do
-      visit contact_path
       page.should have_selector('title', text: 'Contact page')
     end
 
     it "should have the header 'Contact us'" do
-      visit contact_path
       page.should have_selector('h1', text: 'Contact us')
     end
 
     it "should have the form with text 'Contact form'" do
-      visit contact_path
       page.should have_selector('form', text: 'Contact form')
     end
   end
