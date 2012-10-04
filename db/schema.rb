ea3e10112f9c59875bb2279e75c9349fa64442cc
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-04 11:50:13 +0300

    Done with 6.2 Validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003200150) do
+ActiveRecord::Schema.define(:version => 20121004084034) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(:version => 20121003200150) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
