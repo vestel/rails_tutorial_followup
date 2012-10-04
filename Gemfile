d1b2022f55a8b77728bf9cd503660cc29f191b04
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-04 14:36:12 +0300

    Done with Section 6.3.4 User authentication

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'bcrypt-ruby', '3.0.1'
 
 gem 'execjs'
 gem 'therubyracer', :platforms => :ruby
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,20 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50}
+  validates :password, presence: true, length: { minimum: 6}
+  validates :password_confirmation, presence: true, length: {minimum: 6}
+  validates_confirmation_of :password
+
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
-  before_save { |user| user.email = email.downcase }
-
 end
