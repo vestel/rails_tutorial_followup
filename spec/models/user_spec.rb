cbd05c09707d9a505d558c563c26e43dd2909fe0
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-18 18:49:16 +0300

    Finished Section 11.1 Following models/relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -132,6 +132,12 @@ describe User do
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   describe "micropost associations" do
 
@@ -180,5 +186,21 @@ describe User do
     it { should be_admin }
   end
 
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
 
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
 end
