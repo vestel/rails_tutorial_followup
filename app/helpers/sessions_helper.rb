28aa77b69ffbc650c8570d2782865a1cbd1cd446
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-18 17:53:55 +0300

    Finished Section 10.3.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,9 +17,15 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
-
   def signed_in?
     !current_user.nil?
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def sign_out
