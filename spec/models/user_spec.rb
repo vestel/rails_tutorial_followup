956d319aa33c332dc55105b82c16da5a7879ebfe
Aleksandrs Zdancuks <vestel@blog.copperred.net>
2012-10-19 17:04:40 +0300

    Finished Section 11

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -167,9 +167,20 @@ describe User do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
 
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
 
   end
